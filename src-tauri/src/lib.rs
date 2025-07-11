c40f61d40db4d453475501ee88042d2e2080daf6
Carlos Barboza <barbozacarlos678@gmail.com>
2025-07-11 15:09:47 -0300

    Init project.

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,27 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let migrations = vec![Migration {
+        version: 0,
+        description: "proof_migration",
+        sql: "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT);",
+        kind: MigrationKind::Up,
+    }];
+
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:personalnotes.db", migrations)
+                .build(),
+        )
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
